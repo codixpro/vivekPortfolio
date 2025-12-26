438c32b7352aef008696873873baca0daaed1c80
user.name like "Asif Ekhlaque <asifekhlaque33@gmail.com>
2025-12-26 21:33:07 +0530

    Skill

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -2,9 +2,9 @@ import { MoveRight ,FileText } from 'lucide-react';
 
 const Skills = ({key, title, description, categories, categories2 ,link}) => {
   return (
-    <a href={link} target="_blank" key={key}>
+    <a  key={key}>
       <div className="flex items-center justify-center gap-8 pt-12">
-        <div className="text-white w-[350px] border border-orange-500 p-4 rounded-2xl hover:shadow-lg hover:shadow-orange-500/50 hover:scale-105 transition-all duration-200">
+        <div href={link} target="_blank" className="text-white w-[350px] border border-orange-500 p-4 rounded-2xl hover:shadow-lg hover:shadow-orange-500/50 hover:scale-105 transition-all duration-200">
           <h1 className="text-2xl font-bold">{title}</h1>
           <p className="mt-3 text-xs">{description}</p>
           <div className="flex gap-2 mt-3.5">
